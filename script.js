e37707b60b0b401b9adbee92e98dea08f8b760d6
cssmith07060 <cssmith07060@gmail.com>
2022-09-16 14:40:30 -0400

    james brown

---
+++ script.js
@@ -0,0 +1,18 @@
+const checkbox = document.getElementById('checkbox');
+const submitBtn = document.querySelector('button [type=submit]'); 
+
+//disable checkbox
+checkbox.disabled = true;
+submitBtn.disabled = true;
+
+const elements = document.querySelectorAll(".element");
+let selectColor = document.getElementById('SelectColor');
+selectColor = document.querySelector('SelectColor');
+
+elements.forEach(function (element) {
+    const color = getRandomColor
+});
+
+function getRandomColor() {
+    
+}
